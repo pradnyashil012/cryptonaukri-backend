bca621af9d1e26d78ff49df59268f8012d5c8aae
unknown <anshgarewal7@gmail.com>
2022-02-19 12:53:42 +0530

    changes pushed !

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require("express")
 const path = require('path')
 require('dotenv').config()
-// const connectDB = require('./connection/connect')
+    // const connectDB = require('./connection/connect')
 const app = express()
 const port = process.env.PORT || 3001;
 const cors = require("cors")
 const mongoose = require('mongoose')
 
+const admin_business = require("./routes/admin/business/business")
+const admin_internships = require("./routes/admin/internships/internships")
+const admin_jobs = require("./routes/admin/jobs/jobs")
+
+const business_authentication = require("./routes/business/authentication/authentication")
+const business_jobs = require("./routes/business/jobs/jobs")
+const business_interships = require("./routes/business/internships/internships")
+
+const user_authentication = require("./routes/user/authentication/authentication")
+const user_interships = require("./routes/user/internships/internships")
+const user_jobs = require("./routes/user/jobs/jobs")
+const user_resume = require("./routes/user/resume/resume")
 
 
 
-const login = require("./routes/backend_login")
-const job = require("./routes/jobs")
 
 
 
@@ -20,9 +30,18 @@ app.use(express.json())
 app.use(cors())
 
 
-app.use(job)
-app.use(login)
- 
+
+app.use(admin_business)
+app.use(admin_internships)
+app.use(admin_jobs)
+app.use(business_authentication)
+app.use(business_jobs)
+app.use(business_interships)
+app.use(user_authentication)
+app.use(user_interships)
+app.use(user_jobs)
+app.use(user_resume)
+
 
 // if (process.env.NODE_ENV === 'production') {
 
@@ -43,7 +62,7 @@ app.get("/", (req, res) => {
     res.send("APIs are running!")
 })
 
-const start = async () => {
+const start = async() => {
     try {
         await mongoose.connect("mongodb+srv://cryptoNaukri:CN2022@cryptonaukri.psxhi.mongodb.net/cryptoNaukriDb", {
             useNewUrlParser: true,
