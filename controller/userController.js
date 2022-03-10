6b503aaa80a17b11b9e1bdcc83f14f36cb0dbc7f
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-03-10 23:10:51 +0530

    Added OTP support and Made things more specific

--- controller/userController.js
+++ controller/userController.js
@@ -1,32 +1,94 @@
 const userDatabase = require("../models/user/userSchema");
+const otpDatabase = require("../models/otpModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 
-
-exports.userSignup = async (req,res)=>{
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password , salt );
-    const userDataToBeSaved = {
-        firstName : req.body.firstName,
-        lastName : req.body.lastName,
-        email : req.body.email,
-        password : hashedPassword,
-        location : req.body.location,
-        phoneNumber : req.body.phoneNumber
-    };
-    try{
-        await  userDatabase.create(userDataToBeSaved);
-        return res.status(201).json({
-            errorCode : null,
-            userAdded : true,
-            message : "user has been created successfully"
+exports.sendOTP = async (req,res)=>{
+    const userPresenceCheck = await userDatabase.findOne({email : req.query.email});
+    if(!userPresenceCheck){
+        const transporter = nodemailer.createTransport({
+            service:"gmail",
+            auth : {
+                user : process.env.EMAIL,
+                pass: process.env.PASSWORD
+            }
         });
-    }catch (error) {
+        const otp = Math.floor(Math.random()*10000);
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: req.query.email,
+            subject: 'Email verification for Cryptonaukri.com',
+            html: `
+               <h2>Thanks for registering </h2>
+               <h4> Please verify your mail to continue...</h4>
+               <h4>${otp}</h4>
+            `
+        }
+        const otpData = {
+            otp,
+            emailAssociated : req.query.email
+        }
+        await otpDatabase.create(otpData);
+        transporter.sendMail(mailOptions,(err,data)=>{
+            if(err){
+                return res.status(400).json({
+                    code : "OTP_FAILED",
+                    otpSent : false,
+                    message : "Failed To send OTP"
+                });
+            }else{
+                return res.status(200).json({
+                    code : "OTP_SENT",
+                    otpSent : true,
+                    message : "OTP sent"
+                });
+            }
+        });
+    }else{
         return res.status(400).json({
-            errorCode : "DUPLICATE",
+            code : "DUPLICATE",
             userAdded : false,
             message : "Email ID already exists"
+        });
+    }
+}
+
+
+exports.userSignup = async (req,res)=>{
+    const getOTP = await otpDatabase.findOne({email : req.body.email});
+    if(getOTP.otp === req.body.otp){
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password , salt );
+        const userDataToBeSaved = {
+            firstName : req.body.firstName,
+            lastName : req.body.lastName,
+            email : req.body.email,
+            password : hashedPassword,
+            location : req.body.location,
+            phoneNumber : req.body.phoneNumber
+        };
+        try{
+            await userDatabase.create(userDataToBeSaved);
+            res.status(201).json({
+                code : "USER_ADDED",
+                userAdded : true,
+                message : "user has been added successfully"
+            });
+            await otpDatabase.deleteOne({email:req.body.email});
+        }catch (error) {
+            // Need to make this error more specific.
+            res.status(400).json({
+                code : "DUPLICATE",
+                userAdded : false,
+                message : "Email ID already exists"
+            });
+        }
+    }else{
+         res.status(400).json({
+            code : "WRONG_OTP",
+            userAdded : false,
+            message : "Wrong OTP"
         });
     }
 }
@@ -36,7 +98,7 @@ exports.userLogin = async (req,res)=>{
     if(!user){
         return res.status(404).json({
             userLoggedIn : false ,
-            errorCode : "NOT_FOUND" ,
+            code : "NOT_FOUND" ,
             message : "user with such username does not exist"
         });
     }
@@ -44,7 +106,7 @@ exports.userLogin = async (req,res)=>{
     if(!validatePassword){
         return res.status(400).json({
             userLoggedIn : false ,
-            errorCode : "WRONG_PASSWORD",
+            code : "WRONG_PASSWORD",
             message : "user's entered password was wrong"
         });
     }
@@ -57,7 +119,9 @@ exports.userLogin = async (req,res)=>{
         "Authorization" : token
     }).json({
         userLoggedIn : true ,
-        errorCode : null ,
+        code : "LOGGED_IN" ,
         message : "User Logged In Successfully"
     });
 }
+
+
