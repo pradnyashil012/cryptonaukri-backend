fca08bc27b3c1f84f1379aab511a9d90b649d36e
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-03-10 23:11:04 +0530

    Added new routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const {userSignup , userLogin} = require("../controller/userController");
+const {userSignup , userLogin, sendOTP} = require("../controller/userController");
+const {verifyJWT} = require("../middleware/jwtAuthentication");
 
 router.route("/signup")
     .post(userSignup);
 router.route("/login")
     .post(userLogin);
+router.route("/otp")
+    .get(sendOTP);
+
+//Test Route
+router.route("/test")
+    .get(verifyJWT , async (req, res) => {return res.json(req.user)});
 
 module.exports = router;
