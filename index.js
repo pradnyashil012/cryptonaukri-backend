721244fd848ca2b80bd13ddf79b601598b336f33
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-03-17 05:06:28 +0530

    updated businessSchema.js

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const database = require("./configuration/databaseConfig");
 const cors = require("./middleware/cors");
 //Routes
 const userRoute = require("./routes/userRoute");
+const businessRoute = require("./routes/businessRoute");
+
 // const admin_business = require("./routes/admin/business/business")
 // const admin_internships = require("./routes/admin/internships/internships")
 // const admin_jobs = require("./routes/admin/jobs/jobs")
 // const admin_users = require("./routes/admin/users/user")
 //
-// const business_authentication = require("./routes/business/authentication/authentication")
 // const business_jobs = require("./routes/business/jobs/jobs")
 // const business_interships = require("./routes/business/internships/internships")
 //
-// const user_authentication = require("./routes/user/authentication/authentication")
 // const user_interships = require("./routes/user/internships/internships")
 // const user_jobs = require("./routes/user/jobs/jobs")
 // const user_resume = require("./routes/user/resume/resume")
@@ -29,6 +29,7 @@ app.use(cors);
 
 //using Routes
 app.use("/api/v1/user",userRoute);
+app.use("/api/v1/business",businessRoute);
 
 
 database()
