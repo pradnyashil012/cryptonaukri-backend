c8439b5683976c8ab393a36943ca559a13e06a93
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-03-20 22:20:43 +0530

    updated resume schema

--- models/user/userResumeSchema.js
+++ models/user/userResumeSchema.js
@@ -1,29 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const resume_schema = mongoose.model('resume_schema', {
-    userId: {
+const options = {_id : false};
+
+const educationSubSchema = new mongoose.Schema({
+    institutionName : String,
+    startingDate :String,
+    endingDate:String,
+    coursePursued:String,
+    marksScored:String
+},options);
+const jobsSubSchema = new mongoose.Schema({
+    companyName : String ,
+    startingDate  : String ,
+    endingDate : String ,
+    description : String,
+    role : String
+},options);
+const internshipSubSchema = new mongoose.Schema({
+    companyName : String ,
+    startingDate  : String ,
+    endingDate : String ,
+    description : String,
+    role : String
+},options);
+const coursesSubSchema = new mongoose.Schema({
+    name : String ,
+    startingDate : String,
+    endingDate : String ,
+    description : String
+},options)
+
+const projectSubSchema = new mongoose.Schema({
+    name : String ,
+    startingDate : String,
+    endingDate : String ,
+    description : String ,
+    projectLink : String
+},options)
+
+const schema = new mongoose.Schema({
+    userAssociated: {
         type: mongoose.Schema.Types.ObjectId,
-        ref : "user"
+        ref : "user",
+        unique : true
     },
-    education: {
-        type: [String]
-    },
-    jobs: {
-        type: [String]
-    },
-    internships: {
-        type: [String]
-    },
-    training: {
-        type: [String]
-    },
-    projects: {
-        type: [String]
-    },
-    skills: {
-        type: [String]
-    }
+    education: [educationSubSchema],
+    jobs : [jobsSubSchema],
+    internships : [internshipSubSchema],
+    courses: [coursesSubSchema],
+    projects: [projectSubSchema],
+    skills: [{
+        type: String
+    }]
+});
 
-})
-
-module.exports = resume_schema
+module.exports = mongoose.model("userResume",schema);
