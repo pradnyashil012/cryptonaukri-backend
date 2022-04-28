a2c3bbaa13c20c6eeb7f9ca82d4a289a5e6a183d
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-04-28 21:16:58 +0530

    Added Linkedin's OAuth

--- controller/userSignupOAuth.js
+++ controller/userSignupOAuth.js
@@ -123,7 +123,11 @@ exports.linkedinOAuthCall = async (req,res)=>{
 exports.linkedinUserInfo = async (req,res)=>{
     try{
         const code = req.query.code;
-        const accessTokenResponse = await axios.post(`https://www.linkedin.com/oauth/v2/accessToken?client_id=${process.env.LINKEDIN_CLIENT_ID}&client_secret=${process.env.LINKEDIN_CLIENT_SECRET}&redirect_url=${process.env.LINKEDIN_REDIRECT}&code=${code}&grant_type=authorization_code`);
+        const accessTokenResponse = await axios.post(`https://www.linkedin.com/oauth/v2/accessToken?client_id=${process.env.LINKEDIN_CLIENT_ID}&client_secret=${process.env.LINKEDIN_CLIENT_SECRET}&redirect_url=${process.env.LINKEDIN_REDIRECT}&code=${code}&grant_type=authorization_code`,{
+            headers : {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        });
         let accessToken = accessTokenResponse.data;
 
         console.log(accessToken);
