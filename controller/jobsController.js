dc02196f5cb7280c3837a15b66d5b3c9340fbd3e
Enforc3rr <prateekkumartiwari01@gmail.com>
2022-05-30 12:08:45 +0530

    updated

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -176,12 +176,6 @@ exports.changeJobApplicationStatus = async (req,res)=>{
                 sendEmailAfterApplicationStatusChange(application.userDetails,jobToWhichApplicationBelongs.postedByDetails.companyName,req.query.status);
             }catch (e) {
                 console.log(e);
-                return res.status(200).json({
-                    code : "STATUS_CHANGED",
-                    statusChange : true ,
-                    message : "Status of the candidate changed" ,
-                    data : updatedApplication
-                });
             }
 
             return res.status(200).json({
